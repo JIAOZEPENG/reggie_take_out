b15d3768ea1fba404f0299d3986f4e2c7b9f4d74
JIAOZEPENG <2317104867@qq.com>
2023-04-07 10:36:27 +0800

    搭建测试

--- src/main/java/com/buba/comtroller/EmployeeController.java
+++ src/main/java/com/buba/comtroller/EmployeeController.java
@@ -3,8 +3,9 @@ package com.buba.comtroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 @RequestMapping("/employee")
 public class EmployeeController {
 
