689c087330901891a85dfd54d388d134e148ea6a
JIAOZEPENG <2317104867@qq.com>
2023-04-10 14:52:30 +0800

    菜品分页查询

--- src/main/java/com/buba/comtroller/CategoryController.java
+++ src/main/java/com/buba/comtroller/CategoryController.java
@@ -1,15 +1,14 @@
 package com.buba.comtroller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.buba.pojo.Category;
 import com.buba.service.CategoryService;
 import com.buba.utils.R;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -18,11 +17,29 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    //新增分类
+    //新增菜品分类
     @PostMapping
     public R<String> save(@RequestBody Category category){
         log.info("category:{}",category);
         categoryService.save(category);
         return R.success("新增分类成功");
+    }
+
+    /**
+     * 菜品分页查询
+     * @param page 页数
+     * @param pageSize  每页个数
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize){
+        //分页构造器
+        Page<Category> pageInFo = new Page<>(page, pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加排序条件，根据sort进行排序
+        queryWrapper.orderByAsc(Category::getSort);
+        //分页查询
+        categoryService.page(pageInFo, queryWrapper);
+        return R.success(pageInFo);
     }
 }
